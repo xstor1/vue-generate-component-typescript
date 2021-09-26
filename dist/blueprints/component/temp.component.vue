a009ba65efb7f0536700efdbe73650c1f324eb07
Vojtěch Štor <vojtastor32@gmail.com>
2021-09-26 23:31:01 +0200

    change default template increase version to 1.1.1

--- dist/blueprints/component/temp.component.vue
+++ dist/blueprints/component/temp.component.vue
@@ -1,12 +1,12 @@
 <template src="./{{ (name.split('/')[name.split('/').length-1]) | kebabCase }}.component.html"></template>
 
 <script lang="ts">
-import {Options, Vue} from 'vue-class-component';
+import { Options, Vue } from 'vue-class-component'
+
 @Options({})
 export default class {{ name.split('/')[name.split('/').length-1] }}Component extends Vue {
 
 }
 </script>
-
 
 <style src="./{{ (name.split('/')[name.split('/').length-1]) | kebabCase }}.component.scss" scoped lang="scss"></style>
